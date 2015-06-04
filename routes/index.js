ef5f45ee382e633e32758c7ac42defc90a9bbdf8
tianxiangbing <55342775@qq.com>
2015-06-04 14:56:19 +0800

    xx

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,8 @@
-var db = require('../db/mysql');
 var express = require('express');
 var router = express.Router();
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
-	/* open db 
-	db.open(function() {
-		db.collection('users', {
-			safe: true
-		}, function(err,collection) {
-			collection.find().toArray(function(e,docs){
-				if (e) throw e;
-				console.log(docs);
-				res.render('index',{users:docs});
-			});
-		});
-	});
-	*/
 	res.render('index',{title:"聊天室."});
 });
 
