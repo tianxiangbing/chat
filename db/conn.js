134d83b7318db902490117a61504501b36746369
tianxiangbing <55342775@qq.com>
2015-06-04 15:41:31 +0800

    x

--- db/conn.js
+++
@@ -1,7 +0,0 @@
-var mongodb = require('mongodb');
-var mongodbServer = new mongodb.Server('localhost', 27017, {
-	auto_reconnect: true,
-	poolSize: 10
-});
-var db = new mongodb.Db('chat', mongodbServer);
-module.exports = db
--- routes/test.js
+++ routes/test.js
@@ -1,21 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db/conn');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	db.open(function() {
-		db.collection('users', {
-			safe: true
-		}, function(err,collection) {
-			collection.find().toArray(function(e,docs){
-				if (e) throw e;
-				console.log(docs);
-				res.render('test',{users:docs});
-			});
-		});
-	});
-	//res.render('test');
+	res.render('test');
 });
 
 module.exports = router;
